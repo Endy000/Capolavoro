a09ff41f6a215c7224693cea147782dd36539703
Endy000 <168668265+Endy000@users.noreply.github.com>
2024-05-27 18:02:21 +0200

    Update Sami.js prova collegamento

--- Capolavoro_Lembo_Samuele/routes/Sami.js
+++ Capolavoro_Lembo_Samuele/routes/Sami.js
@@ -1,8 +1,21 @@
 const express = require('express');
 let route = express.Router();
+const { exec } = require('child_process');
 
-route.get("/", (req, res) => {
-    res.render("");
+app.get('/', (req, res) => {
+  exec('cd /Sami_News && node server.js', (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Errore durante l'avvio della seconda app: ${error.message}`);
+      return res.status(500).send('Errore durante l\'avvio della seconda app');
+    }
+      
+    if (stderr) {
+      console.error(`stderr: ${stderr}`);
+    }
+
+    console.log(`stdout: ${stdout}`);
+    res.send('Seconda applicazione avviata con successo');
+  });
 });
 
 module.exports = route;
